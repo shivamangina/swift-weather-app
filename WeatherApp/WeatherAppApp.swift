a902c8e60be5d52473e6fc0addc3b640e1aa4bee
Raghu <161108407+0x00raghu@users.noreply.github.com>
2024-02-25 17:29:53 +0530

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Shiva Kumar Mangina on 25/02/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
